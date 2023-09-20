547fa3123375861a8dd99e56ed91105ddcbf839f
youngwoo98 <ykim846@wisc.edu>
2023-09-20 06:01:10 +0000

    commiting all 6 files for submission

---
+++ count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://pages.cs.wisc.edu/~harter/cs544/data/hdma-wi-2021.zip
+
+unzip hdma-wi-2021.zip
+
+cat hdma-wi-2021.csv | grep "Multifamily" | wc -l
+
+rm hdma-wi-2021.zip
+rm hdma-wi-2021.csv
