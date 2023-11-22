155d05e0a7e83e31481472328b9e0bae0f3b8bf8
Jaesung Lim <jlim83@wisc.edu>
2023-11-22 23:10:18 +0000

    finished cleaning file

--- nb/.ipynb_checkpoints/server-checkpoint.py
+++ nb/.ipynb_checkpoints/server-checkpoint.py
@@ -1,8 +1,8 @@
 import grpc
 import station_pb2_grpc, station_pb2
 from concurrent import futures
-from cassandra.cluster import Cluster
-from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster, NoHostAvailable
+from cassandra import ConsistencyLevel, Unavailable
 
 class StationRecord:
     def __init__(self, tmin, tmax):
@@ -12,46 +12,46 @@ class StationRecord:
 class StationServer(station_pb2_grpc.StationServicer):
     def __init__(self):
         cluster = Cluster(['p6-db-1', 'p6-db-2', 'p6-db-3'])
-        cass = cluster.connect('weather')
-        insert_statement = cass.prepare("""
+        self.cass = cluster.connect('weather')
+        self.insert_station = self.cass.prepare("""
         INSERT INTO stations (id, date, record)
         VALUES (?, ?, ?)
         """)
-        insert_statement.consistency_level = ConsistencyLevel.ONE
-        max_statement = cass.prepare("""
+        self.insert_station.consistency_level = ConsistencyLevel.ONE
+        self.max_statement = self.cass.prepare("""
         SELECT MAX(record.tmax) FROM stations WHERE id = ?
         """)
-        max_statement.consistency_level = ConsistencyLevel.ONE
-        cass.cluster.register_user_type('weather', 'station_record', StationRecord)
+        self.max_statement.consistency_level = ConsistencyLevel.ONE
+        self.cass.cluster.register_user_type('weather', 'station_record', StationRecord)
     
     def RecordTemps(self, request, context):
         try:
             record = StationRecord(tmin=request.tmin, tmax=request.tmax)
-            cass.execute(insert_statement, (request.station, request.date, record))
+            self.cass.execute(self.insert_station, (request.station, request.date, record))
             return station_pb2.RecordTempsReply(error = "")
-        except Unavailable as e:
-            return station_pb2.RecordTempsReply(error= f'need {e.required_replicas} replicas, but only have {e.alive_replicas}')
-        except NoHostAvailable as e:
-            for evalue in e.errors.values():
+        except Unavailable as ue:
+            return station_pb2.RecordTempsReply(error= f'need {ue.required_replicas} replicas, but only have {ue.alive_replicas}')
+        except NoHostAvailable as ne:
+            for evalue in ne.errors.values():
                 return station_pb2.RecordTempsReply(error= f'need {evalue.required_replicas} replicas, but only have {evalue.alive_replicas}')
         except Exception as e:
             return station_pb2.RecordTempsReply(error=str(e))
             
     def StationMax(self, request, context):
         try:
-            result = cass.execute(max_statement, (request.station, ))
+            result = self.cass.execute(self.max_statement, (request.station, ))
             row = result.one()
             if row:
-                return station_pb2.StationMaxReply(tmax = row[0], error=str(e))
+                return station_pb2.StationMaxReply(tmax = row[0], error="")
             else:
-                return station_pb2.RecordTempsReply(tmax = 0, error= "No data")
-        except Unavailable as e:
-            return station_pb2.RecordTempsReply(tmax = 0, error= f'need {e.required_replicas} replicas, but only have {e.alive_replicas}')
-        except NoHostAvailable as e:
-            for evalue in e.errors.values():
-                return station_pb2.RecordTempsReply(tmax = 0, error= f'need {evalue.required_replicas} replicas, but only have {evalue.alive_replicas}')
+                return station_pb2.StationMaxReply(tmax = 0, error= "No data")
+        except Unavailable as ue:
+            return station_pb2.StationMaxReply(tmax = 0, error= f'need {ue.required_replicas} replicas, but only have {ue.alive_replicas}')
+        except NoHostAvailable as ne:
+            for evalue in ne.errors.values():
+                return station_pb2.StationMaxReply(tmax = 0, error= f'need {evalue.required_replicas} replicas, but only have {evalue.alive_replicas}')
         except Exception as e:
-            return station_pb2.RecordTempsReply(tmax = 0, error=str(e))
+            return station_pb2.StationMaxReply(tmax = 0, error=str(e))
             
 if __name__ == '__main__':
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4), options=(('grpc.so_reuseport', 0),))
